4d6cd89da57c50dc14752164d05cc747d46ffe9f
Ahmed Zougari <zougariahmed007@gmail.com>
2023-06-15 17:45:54 +0100

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { Icons } from '@/components/Icons'
+import { buttonVariants } from './ui/Button'
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-zinc-300 z-10 py-2">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+        {/* logo */}
+        <Link href="/" className="flex gap-2 items-center">
+          <Icons.logo className="h-2 w-8 sm:h-6 sm:2-6" />
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            Reddit2
+          </p>
+        </Link>
+
+        {/* search bar */}
+
+        <Link href="/login" className={buttonVariants()}>
+          Login
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useState } from 'react'
+import { Button } from './ui/Button'
+import { signIn } from 'next-auth/react'
+import { Icons } from './Icons'
+import { useToast } from '@/hooks/use-toast'
+
+const UserAuthForm = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const { toast } = useToast()
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true)
+
+    try {
+      await signIn('google')
+    } catch (error) {
+      // toast notification
+      toast({
+        title: 'There was a problem.',
+        description: 'There was an error logging in with Google',
+        variant: 'destructive',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return (
+    <div className="flex justify-center">
+      <Button
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+        // {...{ isLoading }}
+
+        size="sm"
+        className="w-full"
+      >
+        {!isLoading && <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  )
+}
+export default UserAuthForm
