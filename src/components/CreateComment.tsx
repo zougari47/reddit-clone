b674f7e18cb00986af2d4139e2b4dcdd83422a8b
Ahmed Zougari <zougariahmed007@gmail.com>
2023-07-10 17:40:34 +0100

    WIP comment

---
+++ src/components/CreateComment.tsx
@@ -0,0 +1,11 @@
+import { Label } from './ui/label'
+
+const CreateComment = () => {
+  return (
+    <div className="grid w-full gap-1.5">
+      <Label />
+    </div>
+  )
+}
+
+export default CreateComment
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
-import { ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from 'clsx'
 import { formatDistanceToNowStrict } from 'date-fns'
 import locale from 'date-fns/locale/en-US'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
