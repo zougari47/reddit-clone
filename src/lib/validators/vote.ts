d045126759ea0386e4b411f74bf71ba21fff5f4f
Ahmed Zougari <zougariahmed007@gmail.com>
2023-07-11 15:18:02 +0100

    finish reply and voting for comment/reply

--- src/lib/validators/vote.ts
+++ src/lib/validators/vote.ts
@@ -8,8 +8,8 @@ export const PostVoteValidator = z.object({
 export type PostVoteRequest = z.infer<typeof PostVoteValidator>
 
 export const CommentVoteValidator = z.object({
-  postId: z.string(),
-  CommentType: z.enum(['UP', 'DOWN']),
+  commentId: z.string(),
+  voteType: z.enum(['UP', 'DOWN']),
 })
 
-export type CommentRequest = z.infer<typeof CommentVoteValidator>
+export type CommentVoteRequest = z.infer<typeof CommentVoteValidator>
